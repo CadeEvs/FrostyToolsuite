2092c5e8f88bff2baf8e4806c642376d01850c15
Diana Gunasena <dulana57@gmail.com>
2022-05-24 16:13:43 -0400

    [Manager] Fix minor issues with Collections UI (#74)
    * Fix issues with scrolling
    
    * fix selecting mods w/ collections
    
    * use unselectall to deselect

--- FrostyModManager/Windows/MainWindow.xaml.cs
+++ FrostyModManager/Windows/MainWindow.xaml.cs
@@ -1762,5 +1762,22 @@ namespace FrostyModManager
             // focus on tab item
             appliedModsTabItem.IsSelected = true;
         }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (sender is ListBox && !e.Handled)
+            {
+                e.Handled = true;
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                eventArg.Source = sender;
+                var parent = ((Control)sender).Parent as UIElement;
+                parent.RaiseEvent(eventArg);
+            }
+        }
+
+        private void collectionModsList_LostFocus(object sender, RoutedEventArgs e) {
+            ((ListView)sender).UnselectAll();
+        }
     }
 }
