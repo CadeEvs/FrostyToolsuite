5123de3bb2fe1ff8697fbfc28cc159c462fe2092
Dyvine57 <dulana57@gmail.com>
2022-05-24 11:45:12 -0400

    Fix issues with scrolling

--- FrostyModManager/Windows/MainWindow.xaml.cs
+++ FrostyModManager/Windows/MainWindow.xaml.cs
@@ -1752,5 +1752,18 @@ namespace FrostyModManager
             // focus on tab item
             appliedModsTabItem.IsSelected = true;
         }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (sender is ListBox && !e.Handled)
+            {
+                e.Handled = true;
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                eventArg.Source = sender;
+                var parent = ((Control)sender).Parent as UIElement;
+                parent.RaiseEvent(eventArg);
+            }
+        }
     }
 }
