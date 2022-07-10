b12852a34526b2946555b3cc6f22d4cdb324a4e9
August <33284629+CosmicDreamsOfCode@users.noreply.github.com>
2022-07-10 04:58:20 +0100

    make sws read only again in prep for PR

--- FrostyCmd/ProfileCreator.cs
+++ FrostyCmd/ProfileCreator.cs
@@ -964,7 +964,7 @@ namespace FrostyCmd
                 writer.Write(0); // ignored res types
 
                 // Flags (MustAddChunks, EbxVersion, RequiresKey, ReadOnly, EAC)
-                ProfileFlags pf = new ProfileFlags(0, 4, 0, 0, 1);
+                ProfileFlags pf = new ProfileFlags(0, 4, 0, 1, 1);
                 pf.Write(writer);
 
                 blobs.Add(key, writer.ToByteArray());
