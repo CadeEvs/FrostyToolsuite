34b886f8afc50a9a7ef6561abb8a607c0b3b227a
Cade <cadegriffin101@gmail.com>
2022-08-01 08:53:53 -0400

    [ModManager] Fixed conflicts executing on disabled mods

--- FrostyModManager/Windows/MainWindow.xaml.cs
+++ FrostyModManager/Windows/MainWindow.xaml.cs
@@ -1418,7 +1418,7 @@ namespace FrostyModManager
                     for (int i = 0; i < selectedPack.AppliedMods.Count; i++)
                     {
                         FrostyAppliedMod appliedMod = selectedPack.AppliedMods[i];
-                        if (!appliedMod.IsFound && !appliedMod.IsEnabled)
+                        if (!appliedMod.IsFound || !appliedMod.IsEnabled)
                             continue;
 
 
@@ -1525,7 +1525,7 @@ namespace FrostyModManager
             for (int i = 0; i < selectedPack.AppliedMods.Count; i++)
             {
                 FrostyAppliedMod appliedMod = selectedPack.AppliedMods[i];
-                if (!appliedMod.IsFound && !appliedMod.IsEnabled)
+                if (!appliedMod.IsFound || !appliedMod.IsEnabled)
                     continue;
 
                 Binding primaryActionBinding = new Binding("") { Converter = new ModPrimaryActionConverter(), ConverterParameter = appliedMod.Mod.Filename };
