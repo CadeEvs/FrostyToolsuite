22b2483ca62db3530e9ac44940d06174e736ebc8
Brawltendo <38518913+Brawltendo@users.noreply.github.com>
2022-08-23 19:53:23 -0700

    [Schematics] Add math expressions to node header

--- Entities/Shared/Entity/MathOpEntity.cs
+++ Entities/Shared/Entity/MathOpEntity.cs
@@ -25,14 +25,17 @@ namespace LevelEditorPlugin.Entities
 				return outProperties;
 			}
 		}
+		public override IEnumerable<string> HeaderRows => m_header;
 
 		protected List<Property<float>> inProperties = new List<Property<float>>();
 		protected Property<float> outProperty;
+		private List<string> m_header = new List<string>();
 
 		public MathOpEntity(FrostySdk.Ebx.MathOpEntityData inData, Entity inParent)
 			: base(inData, inParent)
 		{
 			SetFlags(EntityFlags.HasLogic);
+			BuildExpressionString();
 			for (int i = 0; i < Data.Operators.Count + 1; i++)
             {
 				int hash = Frosty.Hash.Fnv1.HashString($"In{i}");
@@ -53,14 +56,17 @@ namespace LevelEditorPlugin.Entities
 				{
 					switch (mathOp)
                     {
-						case MathOp.MathOp_Add: retVal += inProperties[i++].Value; break;
-						case MathOp.MathOp_Divide: retVal /= inProperties[i++].Value; break;
-						case MathOp.MathOp_Exponent: retVal = (float)Math.Pow(retVal, inProperties[i++].Value); break;
-						case MathOp.MathOp_Max: retVal = Math.Max(retVal, inProperties[i++].Value); break;
-						case MathOp.MathOp_Min: retVal = Math.Min(retVal, inProperties[i++].Value); break;
-						case MathOp.MathOp_Modulo: retVal %= inProperties[i++].Value; break;
-						case MathOp.MathOp_Multiply: retVal *= inProperties[i++].Value; break;
+						case MathOp.MathOp_Add:		 retVal += inProperties[i++].Value; break;
 						case MathOp.MathOp_Subtract: retVal -= inProperties[i++].Value; break;
+						case MathOp.MathOp_Multiply: retVal *= inProperties[i++].Value; break;
+						case MathOp.MathOp_Divide:	 retVal /= inProperties[i++].Value; break;
+						case MathOp.MathOp_Min:		 retVal =  Math.Min(retVal, inProperties[i++].Value); break;
+						case MathOp.MathOp_Max:		 retVal =  Math.Max(retVal, inProperties[i++].Value); break;
+#if NFS16 || NFS_HEAT
+						case MathOp.MathOp_Atan2:	 retVal =  (float)Math.Atan2(retVal, inProperties[i++].Value); break; 
+#endif
+                        case MathOp.MathOp_Modulo:	 retVal %= inProperties[i++].Value; break;
+						case MathOp.MathOp_Exponent: retVal =  (float)Math.Pow(retVal, inProperties[i++].Value); break;
                     }
 				}
 
@@ -70,6 +76,41 @@ namespace LevelEditorPlugin.Entities
 
             base.OnPropertyChanged(propertyHash);
         }
+
+        public override void OnDataModified()
+        {
+			BuildExpressionString();
+            base.OnDataModified();
+        }
+
+        private void BuildExpressionString()
+        {
+            // get rid of the previous expression
+			m_header.Clear();
+
+			string expr = "In0";
+			for (int i = 0; i < Data.Operators.Count; i++)
+			{
+				switch (Data.Operators[i])
+				{
+					case MathOp.MathOp_Add: expr += $" + In{i + 1}"; break;
+					case MathOp.MathOp_Subtract: expr += $" - In{i + 1}"; break;
+					case MathOp.MathOp_Multiply: expr += $" * In{i + 1}"; break;
+					case MathOp.MathOp_Divide: expr += $" / In{i + 1}"; break;
+					case MathOp.MathOp_Min: expr = $"min({expr}, In{i + 1})"; break;
+					case MathOp.MathOp_Max: expr = $"max({expr}, In{i + 1})"; break;
+#if NFS16 || NFS_HEAT
+					case MathOp.MathOp_Atan2: expr = $"atan2({expr}, In{i + 1})"; break;
+#endif
+					case MathOp.MathOp_Modulo: expr += $" % In{i + 1}"; break;
+					case MathOp.MathOp_Exponent: expr += $" ^ In{i + 1}"; break;
+				}
+				expr = $"({expr})";
+			}
+			expr = $"Expr: {expr}";
+			m_header.Add(expr);
+		}
+
     }
 }
 
