60c823db8698f522762b63ecf199c917626adbe1
August <faintlightswitch@gmail.com>
2022-09-18 19:10:25 +0100

    undo res change

--- FrostySdk/Managers/Entries/ResAssetEntry.cs
+++ FrostySdk/Managers/Entries/ResAssetEntry.cs
@@ -83,20 +83,6 @@ namespace FrostySdk.Managers.Entries
     {
         public override string Type => ((ResourceType)ResType).ToString();
         public override string AssetType => "res";
-        public override string Name
-        {
-            get
-            {
-                // TODO: @techdebt find better method to move blueprint bundles to sub-folder, this will most likely break writing.
-                if (ProfilesLibrary.IsLoaded(ProfileVersion.Battlefield2042) &&
-                    (base.Name.Contains("cd_") || base.Name.Contains("md_")))
-                {
-                    return $"win32/{base.Name}";
-                }
-
-                return base.Name;
-            }
-        }
 
         public ulong ResRid;
         public uint ResType;
