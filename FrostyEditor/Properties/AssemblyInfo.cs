478e926f5db5b4c5558411fd3a3583ec34d51a99
Cade <cadegriffin101@gmail.com>
2022-09-28 22:21:00 -0400

    [Core] Increment version to 1.0.6.1

--- FrostyEditor/Properties/AssemblyInfo.cs
+++ FrostyEditor/Properties/AssemblyInfo.cs
@@ -50,8 +50,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.0.6.1")]
+[assembly: AssemblyFileVersion("1.0.6.1")]
 
 #if FROSTY_DEVELOPER
 [assembly: XmlnsDefinition("FrostyDeveloper", "FrostyEditor.Controls")]
