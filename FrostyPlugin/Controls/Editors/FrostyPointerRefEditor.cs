00f2e6af5f0451fff829bbb8aa623440627f8e56
Dyvine57 <dulana57@gmail.com>
2022-10-11 17:39:22 -0400

    Parse as hex

--- FrostyPlugin/Controls/Editors/FrostyPointerRefEditor.cs
+++ FrostyPlugin/Controls/Editors/FrostyPointerRefEditor.cs
@@ -233,7 +233,7 @@ namespace Frosty.Core.Controls.Editors
                 popup.Items.Filter = (object a) => { return ((PointerRefClassType)a).Name.IndexOf(filterText, StringComparison.OrdinalIgnoreCase) >= 0; };
 
                 // search id instead if valid hex
-                if (uint.TryParse(filterText, out uint uintResult))
+                if (uint.TryParse(filterText, NumberStyles.HexNumber, null, out uint uintResult))
                 {
                     popup.Items.Filter = (object a) => { return ((PointerRefClassType)a).Id.Equals(uintResult); };
 
@@ -247,7 +247,7 @@ namespace Frosty.Core.Controls.Editors
                 // search guid instead if valid guid
                 if (Guid.TryParse(filterText, out Guid guidResult))
                 {
-                    uint.TryParse(filterText.Split('-').Last(), out uint id);
+                    uint.TryParse(filterText.Split('-').Last(), NumberStyles.HexNumber, null, out uint id);
                     popup.Items.Filter = (object a) => { return ((PointerRefClassType)a).Guid.Equals(guidResult) || ((PointerRefClassType)a).Id.Equals(id); };
                 }
             }
