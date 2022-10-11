f2392447e38b27d19de192fab90bc12054ad21c8
NM <35472796+NM-20@users.noreply.github.com>
2022-10-10 20:39:13 -0400

    [Editor & ModManager] Optimizations and improvements to SplashWindow and TaskWindow

---
+++ FrostyEditor/SplashWindowLogger.cs
@@ -0,0 +1,120 @@
+﻿using FrostyEditor.Windows;
+using FrostySdk.Converters;
+using FrostySdk.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Shell;
+
+namespace FrostyEditor
+{
+    internal class SplashWindowLogger : ILogger, INotifyPropertyChanged
+    {
+        private SplashWindow parent;
+
+        private double progress;
+
+        private string status;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The progress of the current task.
+        /// </summary>
+        public double Progress
+        {
+            get
+            {
+                return progress;
+            }
+            set
+            {
+                if (value != progress)
+                {
+                    progress = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// The splash window's status.
+        /// </summary>
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                if (value != status)
+                {
+                    status = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public SplashWindowLogger(SplashWindow inParent)
+        {
+            parent = inParent;
+
+            // Utilize DataBindings to eliminate need for Dispatcher
+            BindingOperations.SetBinding(parent.logTextBox, TextBlock.TextProperty, new Binding("Status")
+            {
+                Source = this
+            });
+            BindingOperations.SetBinding(parent.progressBar, ProgressBar.ValueProperty, new Binding("Progress")
+            {
+                Source = this
+            });
+
+            parent.TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Normal;
+
+            BindingOperations.SetBinding(parent.TaskbarItemInfo, TaskbarItemInfo.ProgressValueProperty, new Binding("Progress")
+            {
+                Converter = new DelegateBasedValueConverter(),
+                ConverterParameter = new Func<object, object>(delegate (object value)
+                {
+                    return (double)value / 100.0;
+                }),
+                Source = this,
+            });
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Log(string text, params object[] vars)
+        {
+            string fullText = string.Format(text, vars);
+
+            if (fullText.StartsWith("progress:"))
+            {
+                fullText = fullText.Replace("progress:", "");
+                Progress = double.Parse(fullText);
+            }
+            else
+            {
+                Status = fullText;
+            }
+        }
+
+        public void LogError(string text, params object[] vars)
+        {
+        }
+
+        public void LogWarning(string text, params object[] vars)
+        {
+        }
+    }
+}
---
+++ FrostySdk/Converters/DelegateBasedValueConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FrostySdk.Converters
+{
+    public class DelegateBasedValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DelegateBasedValueConverter"/> class.
+        /// </summary>
+        public DelegateBasedValueConverter()
+        {
+        }
+
+        /// <summary>
+        /// Converts a given value through a provided <see cref="Action{T}"/>, allowing for custom converters without the need for a new class.
+        /// </summary>
+        /// <param name="value">The value to be used.</param>
+        /// <param name="targetType">Invalid for this converter.</param>
+        /// <param name="parameter">The <see cref="Action{T}"/> to be used.</param>
+        /// <param name="culture">Invalid for this converter.</param>
+        /// <returns>The converted value.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null || !(parameter is Func<object, object>))
+            {
+                throw new ArgumentException("\"parameter\" is null or not of the type \"Func<object, object>\".");
+            }
+
+            return ((Func<object, object>)parameter).Invoke(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
