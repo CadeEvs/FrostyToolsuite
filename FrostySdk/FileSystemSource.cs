86197de2227de536c814baae128e37445dde98c2
NM <35472796+NM-20@users.noreply.github.com>
2022-10-15 14:00:25 -0400

    [Sdk] Separated nested ProfilesLibrary classes and added IsLoaded method (#175)

---
+++ FrostySdk/FileSystemSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrostySdk
+{
+    public struct FileSystemSource
+    {
+        public string Path;
+        public bool SubDirs;
+    }
+}
