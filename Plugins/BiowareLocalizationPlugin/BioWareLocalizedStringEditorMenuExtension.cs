90dd6ae595d632abfa7740b17d0df7f941219a2e
KrrKs <117111846+KrrKsThunder@users.noreply.github.com>
2023-01-06 20:25:51 +0100

    Bw localization plugin v6 on frosty1.6 (#211)
    * Commit full version 1.1.0.5 of the BW Localization Plugin
    
    # Conflicts:
    #	Plugins/BiowareLocalizationPlugin/BiowareLocalizedStringDatabase.cs
    #	Plugins/BiowareLocalizationPlugin/LocalizedStringResource.cs
    
    * Add option to search with decimal text ids
    
    * Prepare data structure for editing declinated adjectives
    
    * Implement DragonAgeDeclinatedAdjectiveTuples- functions missing from last commit
    
    * Prepare writing capability for DA:I declinated crafting name adjectives, bump plugin version to reflect major change in produced mod file
    
    * Add setter and remove methods in resource, fix test method
    
    * Fix incorrectly stated number of declinations
    
    * Add adjective getters to text resources
    
    * Refactor xml export, write declinated adjectives.
    
    * Allow Importing of declinated adjectives from xml file
    
    * Add Toggle and (currently non functional) resource selector in ui
    
    * Fix switching to adjectives
    
    * Enable display of declinated adjectives
    
    * Add first sign of adjective edit window
    
    * Working Edit dialog for adjectives
    
    * Try formatting code in accordance with frosty codingstandards
    
    * Small fixes to the adjective edit window
    
    * Add ReplaceWindow
    
    * Fix higlighting and display issues
    
    * Make replacement window available form generic string editor
    
    * Add settings for xml export

---
+++ Plugins/BiowareLocalizationPlugin/BioWareLocalizedStringEditorMenuExtension.cs
@@ -0,0 +1,28 @@
+﻿using BiowareLocalizationPlugin.Controls;
+using Frosty.Core;
+using FrostySdk;
+
+namespace BiowareLocalizationPlugin
+{
+    public class BioWareLocalizedStringEditorMenuExtension : MenuExtension
+    {
+
+        private const string ITEM_NAME = "Bioware Localized String Editor";
+
+        public override string TopLevelMenuName => "View";
+        
+        public override string SubLevelMenuName => null;
+        public override string MenuItemName => ITEM_NAME;
+
+        public override RelayCommand MenuItemClicked => new RelayCommand((o) =>
+        {
+            if (ProfilesLibrary.DataVersion == (int)ProfileVersion.Anthem)
+            {
+                App.Logger.Log("Not applicable for Anthem, sorry for the inconvenience!");
+                return;
+            }
+            var textDb = (BiowareLocalizedStringDatabase) LocalizedStringDatabase.Current;
+            App.EditorWindow.OpenEditor(ITEM_NAME, new BiowareLocalizedStringEditor(textDb));
+        });
+    }
+}
---
+++ Plugins/BiowareLocalizationPlugin/Controls/ListBoxUtils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BiowareLocalizationPlugin.Controls
+{
+    /// <summary>
+    /// Class with some utility methods for handling io out of listboxes
+    /// </summary>
+    public class ListBoxUtils
+    {
+
+        private ListBoxUtils()
+        {
+            // prevent instantiation
+        }
+
+        public static void SortListIntoListBox<T>(IEnumerable<T> entries, ListBox listBox) where T : IComparable
+        {
+            var entryList = new List<T>(entries);
+            entryList.Sort();
+
+            foreach(T entry in entryList)
+            {
+                listBox.Items.Add(entry);
+            }
+        }
+
+        /// <summary>
+        /// Called to copy the selected values of either listbox to the clipboard.
+        /// </summary>
+        /// <param name="listBoxEventOriginator">The originator of the event</param>
+        /// <param name="eventArgs">The event</param>
+        public static void CopySelectionToClipboard(object listBoxEventOriginator, ExecutedRoutedEventArgs eventArgs)
+        {
+            ListBox origin = (ListBox)listBoxEventOriginator;
+
+            IList selection = origin.SelectedItems;
+
+            if (selection.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (object selected in selection)
+                {
+                    sb.AppendLine(selected.ToString());
+                }
+
+                Clipboard.SetText(sb.ToString());
+            }
+        }
+    }
+}
