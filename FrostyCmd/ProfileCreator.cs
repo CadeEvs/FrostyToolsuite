93d8110d6bf86ec318f402df397331e93fc347bf
Alexis Schwindt <alexis.schwindt@outlook.com>
2023-02-21 09:19:50 -0300

    Updated unbound to be a full profile again

--- FrostyCmd/ProfileCreator.cs
+++ FrostyCmd/ProfileCreator.cs
@@ -1152,8 +1152,8 @@ namespace FrostyCmd
                 writer.Write(0); // shared bundle names
                 writer.Write(0); // ignored res types
 
-                // Flags (MustAddChunks, EbxVersion, RequiresKey, ReadOnly)
-                ProfileFlags pf = new ProfileFlags(0, 6, 1, 1);
+                // Flags (MustAddChunks, EbxVersion, RequiresKey)
+                ProfileFlags pf = new ProfileFlags(0, 6, 1);
                 pf.Write(writer);
 
                 blobs.Add(key, writer.ToByteArray());
