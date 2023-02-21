ae931f358cf7fd84db40a88535439ac30c541319
Alexis Schwindt <alexis.schwindt@outlook.com>
2023-02-21 09:55:54 -0300

    Updated profiles.bin and unbound SDK to be full profiles.

--- FrostyCmd/ProfileCreator.cs
+++ FrostyCmd/ProfileCreator.cs
@@ -1219,6 +1219,7 @@ namespace FrostyCmd
             CreateNFSHeatProfile();
             CreateBFHProfile();
             CreateSWSProfile();
+            CreateNFSUnboundProfile();
 
 #if FROSTY_DEVELOPER
 
@@ -1229,7 +1230,6 @@ namespace FrostyCmd
             CreateBF2042Profile();
             CreateMadden23Profile();
             CreateFifa23Profile();
-            CreateNFSUnboundProfile();
             CreateDeadSpaceProfile();
 
 #endif
