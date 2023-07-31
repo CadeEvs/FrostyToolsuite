ca7cb2fd4099042ec5f41b09be07f99cc22b01c1
BreakfastBrainz2 <100167349+BreakfastBrainz2@users.noreply.github.com>
2023-07-31 21:11:36 +0000

    [Sdk] Added Ps3, Xbox 360, Xbox One, and Switch platform names (#273)

--- FrostySdk/Managers/GamePlatform.cs
+++ FrostySdk/Managers/GamePlatform.cs
@@ -7,7 +7,27 @@ public enum GamePlatform
     /// </summary>
     Win32,
     /// <summary>
+    /// Linux
+    /// </summary>
+    Linux,
+    /// <summary>
+    /// Xbox 360
+    /// </summary>
+    Xenon,
+    /// <summary>
+    /// Xbox One
+    /// </summary>
+    Gen4a,
+    /// <summary>
+    /// Playstation 3
+    /// </summary>
+    Ps3,
+    /// <summary>
     /// Playstation 4
     /// </summary>
-    Gen4b
+    Gen4b,
+    /// <summary>
+    /// Nintendo Switch
+    /// </summary>
+    Nx
 }
