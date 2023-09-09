1ceacf9e96357ea3bd55ba7424dded28972caa8c
Dyvine57 <dulana57@gmail.com>
2023-09-09 15:01:29 -0400

    [Editor] Fix order of import log to be more intuitive (redo of #50)

--- FrostyEditor/Windows/MainWindow.xaml.cs
+++ FrostyEditor/Windows/MainWindow.xaml.cs
@@ -1012,7 +1012,7 @@ namespace FrostyEditor
                 if (assetDefinition.Import(entry, ofd.FileName, filters[ofd.FilterIndex - 1].Extension))
                 {
                     dataExplorer.RefreshItems();
-                    App.Logger.Log("Imported {0} to {1}", entry.Name, ofd.FileName);
+                    App.Logger.Log("Imported {0} into {1}", ofd.FileName, entry.Name);
                 }
             }
         }
