CadeEvs/FrostyToolsuite

commit 64db04cd640d741b2fe78cebec9e42358e190c1d

[DuplicationPlugin] Fixed subworld and texture duping (#294)

Plugins/DuplicationPlugin/DuplicateContextMenuItem.cs
@@ -111,7 +111,7 @@ namespace DuplicationPlugin
 
                 // Add new bundle
                 BundleEntry oldBundle = App.AssetManager.GetBundleEntry(newEntry.AddedBundles[0]);
-                BundleEntry newBundle = App.AssetManager.AddBundle("win32/" + newName, BundleType.SubLevel, oldBundle.SuperBundleId);
+                BundleEntry newBundle = App.AssetManager.AddBundle("win32/" + newName.ToLower(), BundleType.SubLevel, oldBundle.SuperBundleId);
 
                 newEntry.AddedBundles.Clear();
                 newEntry.AddedBundles.Add(App.AssetManager.GetBundleId(newBundle));
@@ -489,19 +489,14 @@ namespace DuplicationPlugin
                 ChunkAssetEntry chunkEntry = App.AssetManager.GetChunkEntry(texture.ChunkId);
 
                 // Duplicate the chunk
-                Guid chunkGuid = App.AssetManager.AddChunk(new NativeReader(texture.Data).ReadToEnd(), null, texture);
-                ChunkAssetEntry newChunkEntry = App.AssetManager.GetChunkEntry(chunkGuid);
+                ChunkAssetEntry newChunkEntry = DuplicateChunk(chunkEntry);
 
                 // Duplicate the res
-                ResAssetEntry newResEntry = App.AssetManager.AddRes(newName, ResourceType.Texture, resEntry.ResMeta, new NativeReader(App.AssetManager.GetRes(resEntry)).ReadToEnd());
+                ResAssetEntry newResEntry = DuplicateRes(resEntry, newName, ResourceType.Texture);
                 ((dynamic)newAsset.RootObject).Resource = newResEntry.ResRid;
                 Texture newTexture = App.AssetManager.GetResAs<Texture>(newResEntry);
-                newTexture.ChunkId = chunkGuid;
+                newTexture.ChunkId = newChunkEntry.Id;
                 newTexture.AssetNameHash = (uint)Utils.HashString(newResEntry.Name, true);
-
-                // Add the new chunk/res entries to the original bundles
-                newResEntry.AddedBundles.AddRange(resEntry.EnumerateBundles());
-                newChunkEntry.AddedBundles.AddRange(chunkEntry.EnumerateBundles());
 
                 // Link the newly duplicates ebx, chunk, and res entries together
                 newResEntry.LinkAsset(newChunkEntry);

commit 84f4f480d927c922a1cba656b7fcc3be9e9f1d95

Resolved an issue caused by PR #294 which made it so that the duplication plugin will lower the name of any duped bundle, including ones from SWBF2 which can include capitalisation.

Plugins/DuplicationPlugin/DuplicateContextMenuItem.cs
@@ -89,7 +89,7 @@ namespace DuplicationPlugin
 
                 // Add new bundle
                 BundleEntry oldBundle = App.AssetManager.GetBundleEntry(newEntry.AddedBundles[0]);
-                BundleEntry newBundle = App.AssetManager.AddBundle("win32/" + newName.ToLower(), BundleType.BlueprintBundle, oldBundle.SuperBundleId);
+                BundleEntry newBundle = App.AssetManager.AddBundle("win32/" + (ProfilesLibrary.DataVersion == (int)ProfileVersion.StarWarsBattlefrontII ? newName : newName.ToLower()), BundleType.BlueprintBundle, oldBundle.SuperBundleId);
 
                 newEntry.AddedBundles.Clear();
                 newEntry.AddedBundles.Add(App.AssetManager.GetBundleId(newBundle));
@@ -111,7 +111,7 @@ namespace DuplicationPlugin
 
                 // Add new bundle
                 BundleEntry oldBundle = App.AssetManager.GetBundleEntry(newEntry.AddedBundles[0]);
-                BundleEntry newBundle = App.AssetManager.AddBundle("win32/" + newName.ToLower(), BundleType.SubLevel, oldBundle.SuperBundleId);
+                BundleEntry newBundle = App.AssetManager.AddBundle("win32/" + (ProfilesLibrary.DataVersion == (int)ProfileVersion.StarWarsBattlefrontII ? newName : newName.ToLower()), BundleType.SubLevel, oldBundle.SuperBundleId);
 
                 newEntry.AddedBundles.Clear();
                 newEntry.AddedBundles.Add(App.AssetManager.GetBundleId(newBundle));
@@ -280,7 +280,7 @@ namespace DuplicationPlugin
             {
                 //2017 battlefront meshes always have lowercase names. This doesn't apply to all games, but its still safer to do so
                 newName = newName.ToLower();
-                
+
                 // Duplicate the ebx
                 EbxAssetEntry newEntry = base.DuplicateAsset(entry, newName, createNew, newType);
                 EbxAsset newAsset = App.AssetManager.GetEbx(newEntry);